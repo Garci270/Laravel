7856e9d0452545ee49b3552529b591f8fde1ffc4
Agustin <agustin@MacBook-Air-de-Agustin.local>
2024-02-16 17:49:13 -0300

    almost finish api project on laravel

---
+++ event-management/app/Http/Controllers/Api/AttendeeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class AttendeeController extends Controller
+{
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    } 
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Event $event)
+    {
+        $attendees = $event->attendees()->latest();
+        $attendees = $this->loadRelationships($attendees);
+        return AttendeeResource::collection($attendees->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Event $event)
+    {
+        $attendee = $this->loadRelationships($event->attendees()->create([
+            'user_id' => 1
+        ]));
+
+        return new AttendeeResource($attendee);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event, Attendee $attendee)
+    {
+        return new AttendeeResource($this->loadRelationships($attendee));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event, Attendee $attendee)
+    {
+        $this->authorize('delete-attendee', [$event, $attendee]);
+        $attendee->delete();
+
+        return response()->json([
+            "message" => "Attendee deleted succesfully"
+        ]);
+    }
+}
